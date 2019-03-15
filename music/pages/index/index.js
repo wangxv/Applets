acb147064f6ec9465dd15d88de0014f1b81767a0
wangxv <1564442827@qq.com>
2019-03-15 11:12:57 +0800

    -w 完善

--- music/pages/index/index.js
+++ music/pages/index/index.js
@@ -4,26 +4,39 @@ const app = getApp()
 
 Page({
   data: {
-    ranklist:[]
+    musicList: [],
+    name: ''
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function (option) {
+
+  },
+  bindinput(e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  searchMusic(e) {
     let _that = this
-   wx.request({
-     url:'https://api.apiopen.top/musicRankings',
-     data:{
-     },
-     success:(res)=>{
-      console.log(res.data.result)
-      _that.setData({
-        ranklist:res.data.result
-      })
-     }
-   })
+    wx.request({
+      url: 'https://api.apiopen.top/searchMusic',
+      data: {
+        name: _that.data.name
+      },
+      success: (res) => {
+       let result = res.data.result
+        result.map(x=>{
+          x.title = x.title.length>10 ? (x.title.slice(0,10)+'...') : x.title
+        })
+        _that.setData({
+          musicList: result
+        })
+      }
+    })
   }
 })
